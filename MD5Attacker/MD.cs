f30e9b77221d714709cdd13f4b76887b68075535
hyfree-msi <wangxianqiangcn@foxmail.com>
2022-01-04 08:27:56 +0800

    攻击代码

---
+++ MD5Attacker/MD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD5Attacker
+{
+    public static class MD
+    {
+        public static string MDString3(this string s, string salt)
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] bytes = Encoding.ASCII.GetBytes(s + salt);
+            byte[] bytes1 = md5.ComputeHash(bytes);
+            byte[] bytes2 = md5.ComputeHash(bytes1);
+            byte[] bytes3 = md5.ComputeHash(bytes2);
+            return bytes3.Aggregate("", (current, b) => current + b.ToString("x2"));
+        }
+        public static string MDString3(this byte[] s, byte[] salt)
+        {
+            using MD5 md5 = MD5.Create();
+            byte[] data=new byte[s.Length+salt.Length];
+            s.CopyTo(data,0);
+            salt.CopyTo(data,s.Length);
+           
+            byte[] bytes1 = md5.ComputeHash(data);
+            byte[] bytes2 = md5.ComputeHash(bytes1);
+            byte[] bytes3 = md5.ComputeHash(bytes2);
+            return bytes3.Aggregate("", (current, b) => current + b.ToString("x2"));
+        }
+    }
+}
---
+++ MD5Attacker/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD5Attacker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //var paps="pass";
+            //var key="key";
+            //var a= paps.MDString3(key);
+            //Console.WriteLine(a);
+
+            //var data1=Encoding.ASCII.GetBytes(paps);
+            //var data2=Encoding.ASCII.GetBytes(key);
+
+            //var hash=data1.MDString3(data2);
+            //Console.WriteLine(hash);
+            //Console.ReadKey();
+
+            var hash = "3b75187f5e19a034b5d0a8d35de5f9b2";
+            var salt = "vBBfYYWkriJvpMtFBL3xhVs9oo5QvbRPxUgCKJqFMF6iZKkhk6eMNqSXDus3WJ/t";
+            var a="null";
+            Task task1 = new Task(() =>
+            {
+                for (long i = 45823062; i < 166_666_666_666; i++)
+                {
+                    var h2 = i.ToString().MDString3(salt);
+                   
+                    if (h2.Equals(hash))
+                    {
+                        a=i.ToString();
+                    }
+                }
+            });
+                 Task task2 = new Task(() =>
+            {
+                for (long i = 166_666_666_666; i < 366_666_666_666; i++)
+                {
+                    var h2 = i.ToString().MDString3(salt);
+                  
+                    if (h2.Equals(hash))
+                    {
+                       a=i.ToString();
+                    }
+                }
+            });
+                  Task task3 = new Task(() =>
+            {
+                for (long i = 366_666_666_666; i < 666_666_666_666; i++)
+                {
+                    var h2 = i.ToString().MDString3(salt);
+                   
+                    if (h2.Equals(hash))
+                    {
+                        a=i.ToString();
+                    }
+                }
+            });
+                 Task task4 = new Task(() =>
+            {
+                for (long i = 666_666_666_666; i < 966_666_666_666; i++)
+                {
+                    var h2 = i.ToString().MDString3(salt);
+                   
+                    if (h2.Equals(hash))
+                    {
+                         a=i.ToString();
+                    }
+                }
+            });
+                  Task task5 = new Task(() =>
+            {
+                for (long i = 966_666_666_666; i < 999_999_999_999; i++)
+                {
+                    var h2 = i.ToString().MDString3(salt);
+                    
+                    if (h2.Equals(hash))
+                    {
+                        a=i.ToString();
+                    }
+                }
+            });
+            task1.Start();
+            task2.Start();
+            task3.Start();
+            task4.Start();
+            task5.Start();
+            Task.WaitAll(task1,task2,task3,task4,task5);
+            Console.WriteLine("答案="+a);
+            Console.ReadKey();
+        }
+    }
+}
